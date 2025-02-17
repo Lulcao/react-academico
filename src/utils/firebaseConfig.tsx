50605c0dae6e92da274eb3a112ad89c8b450edd1
Lulcas <lucscmbhering@gmail.com>
2025-02-17 20:25:34 -0300

    alteracoes para inclusao do firebase.

---
+++ src/utils/firebaseConfig.tsx
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA7NenjAdXXgGgucM0-4btnf7KLuRDUUwQ",
+  authDomain: "controlepres-ff0e6.firebaseapp.com",
+  databaseURL: "https://controlepres-ff0e6-default-rtdb.firebaseio.com",
+  projectId: "controlepres-ff0e6",
+  storageBucket: "controlepres-ff0e6.firebasestorage.app",
+  messagingSenderId: "189898767549",
+  appId: "1:189898767549:web:75e10590326a5fa8980bd4",
+  measurementId: "G-6W6PEZJB90"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
